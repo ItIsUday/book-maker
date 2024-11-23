1f893b1ace4e0c182c0f2061dafa3f6b3f8e235d
Uday <iamuday1618@gmail.com>
2024-11-23 21:23:33 +0530

    Add the rest of the functions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from pypdf import PdfReader
+from itertools import compress, pairwise, cycle
+
+from pypdf import PdfReader, PdfWriter, PageObject
 
 
 def equal_partition(pages):
@@ -23,22 +25,52 @@ def rotate_pages(pages, degrees):
 
     return pages
 
-def rotate_pages(interleaved_pages, degrees):
-    pass
+
+def merge_pages(pages):
+    merged_pages = []
+    for i, (page1, page2) in enumerate(compress(pairwise(pages), cycle((1, 0)))):
+        page_width, page_height = page1.mediabox.width, page1.mediabox.height
+        new_page = PageObject.create_blank_page(None, height=page_width, width=page_height)
+        page1.scale_by(page_height / page_width)
+        page2.scale_by(page_height / page_width)
+        if i % 2 == 0:
+            new_page.merge_translated_page(page1, 0, new_page.mediabox.height / 2)
+            new_page.merge_page(page2)
+        else:
+            new_page.merge_translated_page(page2, 0, new_page.mediabox.height / 2)
+            new_page.merge_page(page1)
+        merged_pages.append(new_page)
+
+    return merged_pages
 
 
-def merge_pages(rotated_pages):
-    pass
+def write_to_file(pages, filename):
+    pdf_writer = PdfWriter()
+    for page in pages:
+        pdf_writer.add_page(page)
+    with open(filename, "wb") as output_file:
+        pdf_writer.write(output_file)
 
 
 def main():
-    reader = PdfReader()
-    pages = reader.pages
+    pdf_filename = "/Users/uarsiker/Downloads/cake.pdf"
+    pdf_reader = PdfReader(pdf_filename)
+    pages = list(pdf_reader.pages)
+
     partition1, partition2 = equal_partition(pages)
-    interleaved_pages = interleave(partition2, partition1)
+    interleaved_pages = interleave(partition1, partition2)
     rotated_pages = rotate_pages(interleaved_pages, 90)
     merged_pages = merge_pages(rotated_pages)
+    for i in range(1, len(merged_pages), 2):
+        merged_pages[i].rotate(180)
+    write_to_file(merged_pages, "output.pdf")
 
 
 if __name__ == '__main__':
     main()
+
+# Design
+# 1) Split pages into two equal partition (add a blank if necessary)
+# 2) Interleave the first partition with the second partition
+# 3) Rotate and Merge every pair of pages
+# 4) Flip every odd page
